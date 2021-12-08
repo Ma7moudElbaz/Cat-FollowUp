28512839c1462e91438687a4d472eca59f21cdf3
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2021-12-08 17:34:11 +0200

    Commit 8/12/2021

---
+++ app/src/main/java/com/example/followup/job_orders/JobOrderDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.followup.job_orders;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.followup.R;
+
+public class JobOrderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_job_order_details);
+    }
+}
--- app/src/main/java/com/example/followup/requests/RequestDetailsActivity.java
+++ app/src/main/java/com/example/followup/requests/RequestDetailsActivity.java
@@ -3,14 +3,23 @@ package com.example.followup.requests;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.followup.R;
+import com.shuhart.stepview.StepView;
 
 public class RequestDetailsActivity extends AppCompatActivity {
+
+    StepView step_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_details);
+
+//        step_view.go(1, true);
+
+
     }
 }
