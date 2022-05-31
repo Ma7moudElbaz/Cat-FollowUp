12ae99079300dfaea2fe10e1dcc4d4df50ab5ddf
Ma7moudElbaz <dev.mahmoud.elbaz@gmail.com>
2022-05-31 16:38:52 +0200

    Commit 31/05/2022

--- app/src/main/java/com/example/followup/utils/GetPath.java
+++ app/src/main/java/com/example/followup/utils/GetPath.java
@@ -25,8 +25,6 @@ public class GetPath {
                 if ("primary".equalsIgnoreCase(type)) {
                     return Environment.getExternalStorageDirectory() + "/" + split[1];
                 }
-
-                // TODO handle non-primary volumes
             }
             // DownloadsProvider
             else if (isDownloadsDocument(uri)) {
